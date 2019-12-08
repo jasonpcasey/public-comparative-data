6bffd2f5766a3c4b1e4742d954b8fa9ba7de3da3
jasonpcasey <jazzerj@gmail.com>
2019-12-08 09:37:35 -0600

    Create insert_detail_data_multi_year_local.py

---
+++ insert_detail_data_multi_year_local.py
@@ -0,0 +1,141 @@
+# coding=utf-8
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+
+import pickle
+
+from sqlalchemy import sql
+
+from base import engine, Session, Base
+from date_dimension import DateRow
+from nsf_herd_academic_fields import NsfHerdAcademicField
+from nsf_herd_federal_agencies import NsfHerdFederalAgency
+from nsf_herd_detail_data import NsfHerdDetail
+
+pd.set_option('display.max_rows', 10)
+
+# constants
+first_year = 2010
+last_year = 2018
+
+# local functions
+def item_recode(col, codings, default_value = None):
+    if default_value == None:
+        answer = col.map(codings, na_action = 'ignore')
+    else:
+        answer = col.map(codings, na_action = 'ignore').fillna(default_value) 
+    return(answer)
+
+for year in np.arange(first_year, last_year + 1):
+    try:
+        spec = 'data/nsf_{}.pickle'.format(year)
+        print('Reading data for fiscal year ending {}:\n\t{}... '.format(year, spec), end='', flush=True)
+        with open(spec, 'rb') as f:
+            herd = pickle.load(f)
+    except Exception as e:
+        print('ERROR.\nFile not downloaded properly.\n\n{}\n'.format(str(e)))
+    else:
+        print('DONE.')
+        herd.info()
+
+    # set date key
+    date_key = '{}-06-30'.format(year)
+
+    # modify data frame to apply needed fixes
+    herd['date_key'] = date_key
+
+    herd.ipeds_unitid = herd.ipeds_unitid.fillna(-1).astype(int)
+    herd.ncses_inst_id = herd.ncses_inst_id.fillna('XXXXXXXX')
+
+    herd = herd[herd.column.isin(['DOD',
+                                'DOE',
+                                'HHS',
+                                'NASA',
+                                'NSF',
+                                'USDA',
+                                'Other agencies',
+                                'State and local government',
+                                'Business',
+                                'Institution funds',
+                                'Nonprofit organziations'])]
+
+
+    herd['agency_key'] = item_recode(herd.column,
+                                    {'DOD': 'DOD',
+                                    'DOE': 'DOE',
+                                    'HHS': 'HHS',
+                                    'NASA': 'NAS',
+                                    'NSF': 'NSF',
+                                    'USDA': 'USD',
+                                    'Other agencies': 'OTH',
+                                    'State and local government': 'SLG',
+                                    'Business': 'BUS',
+                                    'Institution funds': 'INS',
+                                    'All other sources': 'OTH',
+                                    'Nonprofit organziations': 'NPO'},
+                                    'Unknown')
+
+    herd['funding_type'] = herd.questionnaire_no.str[:2]
+
+    herd = herd[herd.funding_type.isin(['09', '11'])]
+
+    herd['funding_type'] = item_recode(herd.funding_type, {'09': 'Federal', '11': 'Non-federal'}, 'Unknown')
+
+    herd['academic_field_key'] = herd.questionnaire_no.str[2:].str.strip()
+    herd = herd[herd.academic_field_key.isin(['A','B01','B02','B03','B04','B05','B06','B07','B08',
+                                            'B09','C01','C02','C03','C04','D01','D02','D03','D04',
+                                            'D05','E','F01','F02','F03','F04','F05','G','H01',
+                                            'H02','H03','H04','H05','I','J01','J02','J03','J04',
+                                            'J05','J06','J07','J08'])]
+
+    herd['data'] = herd.data.fillna(0) * 1000
+
+    # rename columns
+    herd = herd.rename(columns = {'ipeds_unitid': 'unitid',
+                                'data': 'expenditure'})
+
+    # de-duplicate items
+    keepers = ['inst_id',
+            'date_key',
+            'funding_type',
+            'agency_key',
+            'academic_field_key',
+            'ncses_inst_id',
+            'unitid',
+            'expenditure']
+
+    herd = herd[keepers].groupby(['inst_id',
+            'date_key',
+            'funding_type',
+            'agency_key',
+            'academic_field_key',
+            'ncses_inst_id',
+            'unitid']).sum().reset_index()
+
+    herd['expenditure'] = herd.expenditure.fillna(0)
+    herd = herd.fillna(sql.null())
+
+    # insert data into dbo.survey_records
+    session = Session()
+
+    try:
+        print('Attempting to insert {:,} rows for {} into {}.'.format(herd.shape[0], year, NsfHerdDetail.__tablename__))
+        record_deletes = session.query(NsfHerdDetail).filter(NsfHerdDetail.date_key==date_key).delete(synchronize_session=False)
+        session.bulk_insert_mappings(mapper = NsfHerdDetail,
+                                    mappings = herd.to_dict(orient='records'),
+                                    render_nulls = True)
+    except Exception as e:
+        session.rollback()
+        print(str(e))
+        print('No data were altered due to error.')
+    else:
+        session.commit()
+        print('\n{:,} old records were deleted.'.format(record_deletes))
+        print('{:,} new records were inserted.'.format(herd.shape[0]))
+    finally:
+        session.close()
+        session = None
+
+print('\nAll done!')
