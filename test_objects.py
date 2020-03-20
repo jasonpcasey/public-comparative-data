e48fa00deffb8d635c5d4c622c7120c7d5bf491b
Jason Casey <jazzerj@gmail.com>
2020-03-19 21:19:43 -0500

    Update test_objects.py

--- test_objects.py
+++ test_objects.py
@@ -5,12 +5,15 @@ class Dude:
     Name: str = 'The Dude'
     Height: float = 6.0
 
+    def to_dict(self):
+        return self.__dict__
+
 def main():
     dude = Dude('Jeff', 5.96)
     dude2 = Dude('Jeff', 5.96)
 
     print(dude)
-    print(dude.__dict__)
+    print(dude.to_dict())
     print(dude == dude2)
 
 if __name__ == '__main__':
