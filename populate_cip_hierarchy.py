75853db927a5dcf5bf96bcdd595bac98b3a0074e
Jason Casey <jazzerj@gmail.com>
2020-05-10 21:41:37 -0500

    Update populate_cip_hierarchy.py

--- populate_cip_hierarchy.py
+++ populate_cip_hierarchy.py
@@ -34,6 +34,20 @@ cip2 = cip2.rename(
     }
 )
 
+cip6 = cip[cip.cipcode.str.len() == 7][['cipcode', 'title', 'family', 'version']]
+cip6['cip6'] = pd.to_numeric(
+    cip6.cipcode,
+    errors = 'coerce'
+    )
+cip6['cip4'] = cip.cipcode.str[:5]
+cip6['cip2'] = cip.cipcode.str[:2]
+cip6 = cip6.rename(
+    columns={
+        'title': 'cip6_description',
+        'version': 'latest_version',
+        }
+    )
+
 cip4 = cip[cip.cipcode.str.len() == 5][['cipcode', 'title']]
 cip4 = cip4.rename(
     columns={
@@ -42,34 +56,24 @@ cip4 = cip4.rename(
     }
 )
 
-cip6 = cip[cip.cipcode.str.len() == 7][['cipcode', 'title', 'family']]
-cip6['cip6'] = pd.to_numeric(
-    cip6.cipcode,
-    errors = 'coerce'
-    )
-cip6['cip4'] = cip.cipcode.str[:5]
-cip6 = cip6.rename(
-    columns={
-        'family': 'cip2',
-        'title': 'cip6_description'
-        }
-    )
-
 hierarchy = cip6.merge(
     right = cip2,
-    how = 'inner',
+    how = 'left',
     on = 'cip2'
     )
 
 hierarchy = hierarchy.merge(
     right = cip4,
-    how = 'inner',
+    how = 'left',
     on = 'cip4'
     )
 
+hierarchy.cip4_description = hierarchy.cip4_description.fillna('Unknown')
+hierarchy.cip2_description = hierarchy.cip2_description.fillna('Unknown')
+
 try:
-    print('\nPopulating dimension tables.')
-    # insert state data
+    print('Populating dimension table.')
+    # insert cip data
     record_deletes = session.query(Cip).delete(synchronize_session=False)
     df_inserts = session.bulk_insert_mappings(mapper = Cip,
                                               mappings = hierarchy.to_dict(orient='records')),
