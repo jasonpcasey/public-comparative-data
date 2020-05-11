6049de843a576603d4d2d6ae9f7cc5150d0f24b6
Jason Casey <jazzerj@gmail.com>
2020-05-10 21:41:16 -0500

    Update cip_hierarchy.py

--- database/cip_hierarchy.py
+++ database/cip_hierarchy.py
@@ -11,13 +11,14 @@ class Cip(Base):
     """ create columns """
     cipcode = Column(String(7), primary_key = True)
     cip6 = Column(Numeric, nullable = False)
-    cip6_description = Column(String(132), nullable = False)
+    cip6_description = Column(String(500), nullable = False)
     cip4 = Column(Numeric, nullable = False)
-    cip4_description = Column(String(132), nullable = False)
+    cip4_description = Column(String(500), nullable = False)
     cip2 = Column(Integer, nullable = False)
-    cip2_description = Column(String(132), nullable = False)
+    cip2_description = Column(String(500), nullable = False)
+    latest_version = Column(Integer, nullable = False)
 
-    def __init__(self, cipcode, cip6, cip6_description, cip4, cip4_description, cip2, cip2_description):
+    def __init__(self, cipcode, cip6, cip6_description, cip4, cip4_description, cip2, cip2_description, latest_version):
         """ method for instantiating object """
         self.cipcode = cipcode
         self.cip6 = cip6
@@ -26,6 +27,7 @@ class Cip(Base):
         self.cip4_description = cip4_description
         self.cip2 = cip2
         self.cip2_description = cip2_description
+        self.latest_version = latest_version
 
     def __repr__(self):
         """ produces human-readable object call """
@@ -37,5 +39,6 @@ class Cip(Base):
             f'cip4={self.cip4!r}, '
             f'cip4_description={self.cip4_description!r}, '
             f'cip2={self.cip2!r}, '
-            f'cip2_description={self.cip2_description!r})'
+            f'cip2_description={self.cip2_description!r}'
+            f'latest_version={self.latest_version})'
             )
